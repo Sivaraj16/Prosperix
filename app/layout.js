f3ae0bc794da27da07c817cce094f768f7aee62a
SIVARAJ.S <sivaraj16012005@gmail.com>
2025-03-12 12:02:32 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
@@ -6,23 +7,19 @@ import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "Welth",
-  description: "One stop Finance Platform",
-};
-
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <head>
+        <Head>
+          <title>Prosperix</title>
+          <meta name="description" content="One stop Finance Platform" />
           <link rel="icon" href="/logo-sm.png" sizes="any" />
-        </head>
+        </Head>
         <body className={`${inter.className}`}>
           <Header />
           <main className="min-h-screen">{children}</main>
           <Toaster richColors />
-
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
               <p>Made with 💗 by Sivaraj</p>
